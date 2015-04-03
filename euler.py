6595acb6f25b3aee7de293ca5e84b96f5b3fa5b8
Peter Aarestad <paarestad@grubhub.com>
2015-04-03 16:11:31 -0500

    more stuff

--- euler.py
+++ euler.py
@@ -403,3 +403,39 @@ def to_letters(n):
 len_sum = 0
 for n in range(1, 1001):
   len_sum += len(to_letters(n))
+
+triangle_text = ["75",
+"95 64",
+"17 47 82",
+"18 35 87 10",
+"20 04 82 47 65",
+"19 01 23 75 03 34",
+"88 02 77 73 07 63 67",
+"99 65 04 28 06 16 70 92",
+"41 41 26 56 83 40 80 70 33",
+"41 48 72 33 47 32 37 16 94 29",
+"53 71 44 65 25 43 91 52 97 51 14",
+"70 11 33 28 77 73 17 78 39 68 17 57",
+"91 71 52 38 17 14 91 43 58 50 27 29 48",
+"63 66 04 68 89 53 67 30 73 16 69 87 40 31",
+"04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"]
+
+triangle = []
+
+for line in triangle_text:
+  triangle.append(map(int, line.split()))
+
+best_sum = 0
+
+for n in range(2**14-1):
+  # n represents a path, in binary: 0 = "go left", 1 = "go right". 15 total steps per path
+  pos = 0
+  path = []
+  for x in range(15):
+    path.append(triangle[x][pos])
+    pos += n & 1 # "going left" means keeping the same index into the next row
+    n >>= 1
+  sum = reduce(lambda x, y: x + y, path)
+  if sum > best_sum:
+    print path, "gives the sum", sum
+    best_sum = sum
