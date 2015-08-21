289d445ab8effe351d3b3f1d8ff22cf9ccd4fe09
Peter Aarestad <paarestad@grubhub.com>
2015-08-21 15:24:12 -0500

    cleanup

--- euler_89.py
+++ euler_89.py
@@ -45,48 +45,30 @@ def from_roman_numeral(rn):
 
 	return decimal_rep
 
+to_roman_numeral_values = [
+	(1000, 'M'),
+	(900, 'DM'),
+	(500, 'D'),
+	(400, 'CD'),
+	(100, 'C'),
+	(90, 'XC'),
+	(50, 'L'),
+	(40, 'XL'),
+	(10, 'X'),
+	(9, 'IX'),
+	(5, 'V'),
+	(4, 'IV'),
+	(1, 'I'),
+]
 def to_roman_numeral(n):
 	roman_rep = ''
 	while n > 0:
-		if n >= 1000:
-			roman_rep += 'M'
-			n -= 1000
-		elif n >= 900:
-			roman_rep += 'DM'
-			n -= 900
-		elif n >= 500:
-			roman_rep += 'D'
-			n -= 500
-		elif n >= 400:
-			roman_rep += 'CD'
-			n -= 400
-		elif n >= 100:
-			roman_rep += 'C'
-			n -= 100
-		elif n >= 90:
-			roman_rep += 'XC'
-			n -= 90
-		elif n >= 50:
-			roman_rep += 'L'
-			n -= 50
-		elif n >= 40:
-			roman_rep += 'XL'
-			n -= 40
-		elif n >= 10:
-			roman_rep += 'X'
-			n -= 10
-		elif n >= 9:
-			roman_rep += 'IX'
-			n -= 9
-		elif n >= 5:
-			roman_rep += 'V'
-			n -= 5
-		elif n >= 4:
-			roman_rep += 'IV'
-			n -= 4
-		elif n >= 1:
-			roman_rep += 'I'
-			n -= 1
+		for v in to_roman_numeral_values:
+			if n >= v[0]:
+				roman_rep += v[1]
+				n -= v[0]
+				break
+
 	return roman_rep
 
 with open('p089_roman.txt') as numeral_input:
