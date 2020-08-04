a768616a0851927adbc3091b0352286c90422068
Peter Aarestad <peter.aarestad@tempus.com>
2020-08-04 14:35:28 -0500

    Problem 205

--- problem205.py
+++ problem205.py
@@ -5,35 +5,59 @@
 
 # What is the probability that Pyramidal Pete beats Cubic Colin?
 # Give your answer rounded to seven decimal places in the form 0.abcdefg
+import itertools
 
-# PP
-pp_probs = {
-  9: 1,
-  10: 4,
-  11:
-  12:
-  13:
-  14:
-  15:
-  16:
-  17:
-  18:
-  19:
-  20:
-  21:
-  22:
-  23:
-  24:
-  25:
-  26:
-  27:
-  28:
-  29:
-  30:
-  31:
-  32:
-  33:
-  34: 
-  35: 4,
-  36: 1
-}
+pp_probs = { }
+cc_probs = { }
+
+for d1 in range(1, 5):
+    for d2 in range(1, 5):
+        for d3 in range(1, 5):
+            for d4 in range(1, 5):
+                for d5 in range(1, 5):
+                    for d6 in range(1, 5):
+                        for d7 in range(1, 5):
+                            for d8 in range(1, 5):
+                                for d9 in range(1, 5):
+                                    combo_sum = d1+d2+d3+d4+d5+d6+d7+d8+d9
+
+                                    if combo_sum in pp_probs:
+                                        pp_probs[combo_sum] += 1
+                                    else:
+                                        pp_probs[combo_sum] = 1
+
+for d1 in range(1, 7):
+    for d2 in range(1, 7):
+        for d3 in range(1, 7):
+            for d4 in range(1, 7):
+                for d5 in range(1, 7):
+                    for d6 in range(1, 7):
+                        combo_sum = d1+d2+d3+d4+d5+d6
+
+                        if combo_sum in cc_probs:
+                            cc_probs[combo_sum] += 1
+                        else:
+                            cc_probs[combo_sum] = 1
+
+print(pp_probs)
+print(cc_probs)
+
+total_pp_options = sum(pp_probs.values())
+total_cc_options = sum(cc_probs.values())
+print(total_pp_options)
+print(total_cc_options)
+
+denominator = total_pp_options * total_cc_options
+print(denominator)
+
+total_pp_ways_to_win = 0
+
+for cc_sum, cc_sum_combos in cc_probs.items():
+    pp_winners = (pp_sum for pp_sum in pp_probs if pp_sum > cc_sum)
+
+    for winner in pp_winners:
+        total_pp_ways_to_win += cc_sum_combos * pp_probs[winner]
+
+print(total_pp_ways_to_win)
+
+print(total_pp_ways_to_win / denominator)
